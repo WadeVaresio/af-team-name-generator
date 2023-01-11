f1887331a720db7311002907aac0c2e2c34a1dc3
Wade Varesio <wade.varesio@appfolio.com>
2023-01-11 12:16:14 -0800

    wip

--- pun.py
+++ pun.py
@@ -1,6 +1,7 @@
 import heapq
 import silabeador as sil
 import spacy
+import en_core_web_md
 from pyphonetics import RefinedSoundex
 from sys import argv
 
@@ -89,7 +90,8 @@ def compute(name, method, top_n, recurse, puns=[]):
 
 
 # load scorer
-nlp = spacy.load('en_core_web_md')
+#nlp = spacy.load('en_core_web_md')
+nlp = en_core_web_md.load()
 
 if __name__ == "__main__":
   scorer, top_n, recurse = argv[1], int(argv[2]), argv[3]
